886993689cffbf0a986d54a7d35f08135e36c067
Burak Kahraman <49955817+burakkahraman95@users.noreply.github.com>
2026-04-20 05:54:33 -0700

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,118 @@
+function setupGame() {
+
+    // Castle scene
+    let castleScene = {};
+    castleScene.imageSource = './static/images/backdrops/Castle.png';
+    castleScene.title = 'A Huge Stone Castle';
+    setupScene(castleScene);
+
+    // Beach Town scene
+    let beachTownScene = {};
+    beachTownScene.imageSource = './static/images/backdrops/BeachTown.png';
+    beachTownScene.title = 'A Village by the Beach';
+    setupScene(beachTownScene);
+
+    // House scene
+    let houseScene = {};
+    houseScene.imageSource = './static/images/backdrops/House.png';
+    houseScene.title = 'A Cosy Cottage in the Village';
+    setupScene(houseScene);
+
+    // Forest scene
+    let forestScene = {};
+    forestScene.imageSource = './static/images/backdrops/Forest.png';
+    forestScene.title = 'A Dark and Gloomy Forest';
+    setupScene(forestScene);
+
+    // Characters
+    let farmer = {
+        name: 'The Farmer',
+        imageSource: './static/images/people/Farmer.png',
+        dialog1: 'Learn JavaScript? Only if you ask me enough...',
+        dialog2: 'Okay, okay! I am studying JavaScript now...',
+        currentDialog: 'dialog1',
+    };
+
+    let queen = {
+        name: 'The Queen',
+        imageSource: './static/images/people/Queen.png',
+        dialog1: 'Ask The Knight. If The Knight learns, I too shall learn.',
+        dialog2: 'The Knight is starting? I guess is JS for me! (YOU WIN!)',
+        currentDialog: 'dialog1',
+    };
+
+    let knight = {
+        name: 'The Knight',
+        imageSource: './static/images/people/Knight.png',
+        dialog1: 'I only will learn JavaScript if you get The Farmer to first!',
+        dialog2: 'The Farmer is learning? Time to play catch up!',
+        currentDialog: 'dialog1',
+    };
+
+    // Farmer: must be asked 3 times
+    let askCount = 0;
+    farmer.afterTalking = function () {
+        askCount++;
+        if (askCount > 1) {
+            farmer.currentDialog = 'dialog2';
+        }
+    };
+
+    // Knight: works AFTER farmer agrees
+    let knightAskCount = 0;
+    knight.afterTalking = function () {
+        if (farmer.currentDialog === 'dialog2') {
+            knightAskCount++;
+            if (knightAskCount >= 1) {
+                knight.currentDialog = 'dialog2';
+            }
+        }
+    };
+
+    // Queen: works AFTER knight agrees
+    let queenAskCount = 0;
+    queen.afterTalking = function () {
+        if (knight.currentDialog === 'dialog2') {
+            queenAskCount++;
+            if (queenAskCount >= 1) {
+                queen.currentDialog = 'dialog2';
+            }
+        }
+    };
+
+    // Setup all people
+    setupPerson(farmer);
+    setupPerson(knight);
+    setupPerson(queen);
+
+    // Place people in scenes
+    houseScene.addPerson(farmer);
+    forestScene.addPerson(knight);
+    castleScene.addPerson(queen);
+
+    // Castle doors
+    castleScene.addDoor('Exit the castle to nearby beach town', beachTownScene);
+
+    // Beach Town doors (correct order: Forest first, then Castle, then House)
+    beachTownScene.addDoor('Wander off into the forest', forestScene);
+    beachTownScene.addDoor('Enter castle', castleScene);
+    beachTownScene.addDoor('Enter house', houseScene);
+
+    // House doors
+    houseScene.addDoor('Exit house', beachTownScene);
+
+    // Forest doors (correct order: Castle back door first, then Beach town)
+    forestScene.addDoor('Enter the castle back door', castleScene);
+    forestScene.addDoor('Walk back to the beach town', beachTownScene);
+
+    return castleScene;
+}
+
+function startGame() {
+    let castleScene = setupGame();
+    castleScene.showScene('#game');
+    castleScene.showDoors('#game');
+    castleScene.showAllPeople('#game');
+}
+
+startGame();
